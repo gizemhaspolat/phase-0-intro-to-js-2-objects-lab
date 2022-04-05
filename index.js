67acaadf6fc3833d9299cb5e07ab58c59e6ec203
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-04-05 16:28:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let employee = {
+  name: "John",
+  streetAddress: "123 Main St.",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  let newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmployee = { ...employee };
+  delete newEmployee.name;
+  return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee.name;
+  return employee;
+}
